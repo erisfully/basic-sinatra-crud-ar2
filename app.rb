f5e63c19a9c3633eca564f43de2884d3cf1c32e8
Ashley <a.clarkem@gmail.com>
2014-07-14 16:50:12 -0600

    finished log in and log out

--- app.rb
+++ app.rb
@@ -1,19 +1,41 @@
 require "sinatra"
 require "active_record"
 require "./lib/database_connection"
+require "rack-flash"
 
 class App < Sinatra::Application
+  enable :sessions
+  use Rack::Flash
+
+
   def initialize
     super
     @database_connection = DatabaseConnection.establish(ENV["RACK_ENV"])
   end
 
   get "/" do
-    erb :home
+    if session[:id]
+      current_user = @database_connection.sql("SELECT username FROM users WHERE id = #{session[:id]}")[0]["username"]
+      erb :loggedin, :locals => {:current_user => current_user}
+    else
+      erb :home
+    end
+  end
+
+  post "/" do
+    id = @database_connection.sql("SELECT id FROM users WHERE username = '#{params[:username]}'").last["id"]
+    session[:id] = id
+    p id
+    redirect "/"
   end
 
   get "/register" do
     erb :register
   end
 
+  post "/register" do
+    @database_connection.sql("INSERT INTO users (username, password) VALUES ('#{params[:username]}', '#{params[:password]}')")
+    flash[:notice] = "Thank you for registering!"
+    redirect'/'
+  end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -11,10 +11,39 @@ require 'spec_helper'
   feature "Register" do
     scenario "Click register button and see form" do
       visit "/"
+
       click_button("Register")
 
       expect(page).to have_content("Username:")
       expect(page).to have_content("Password:")
+
       expect(page).to have_button("Submit")
-      end
+
+      fill_in("username", :with => "Ash")
+      fill_in("password", :with => "123")
+
+      click_button("Submit")
+
+
+      expect(page).to have_content("Thank you for registering!")
+
+      visit "/"
+
+      expect(page).to_not have_content("Thank you for registering!")
+
+      visit "/"
+
+      fill_in("username", :with => "Ash")
+      fill_in("password", :with => "123")
+
+      click_button("Login")
+
+      save_and_open_page
+
+      expect(page).to have_content("Welcome, Ash!")
+
+      expect(page).to_not have_button("Register", "Login")
+      expect(page).to have_button("Logout")
+
     end
+  end
