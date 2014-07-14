1c48badb3e92980b44cfdbb690dbfe10469da0cd
Ashley <a.clarkem@gmail.com>
2014-07-14 11:53:55 -0600

    homepage with register button

---
+++ spec/app_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+  feature "Homepage" do
+    scenario "Has registration button" do
+
+    visit "/"
+    expect(page).to have_button("Register")
+    end
+  end
+
+  feature "Register" do
+    scenario "Click register button and see form" do
+      visit "/"
+      click_button("Register")
+
+      expect(page).to have_content("Username:")
+      expect(page).to have_content("Password:")
+      end
+    end
